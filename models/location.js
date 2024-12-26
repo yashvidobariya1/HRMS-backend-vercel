0b42b8da5a37305828ab968ba175e93381b32f8b
HeniBalar <henishreesoftic@gmail.com>
2024-12-26 02:07:27 -0700

    add schema and api for location

---
+++ models/location.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const locationSchema = new mongoose.Schema({
+    isDeleted: {
+        type: Boolean, default: false
+    },
+    companyName: String,
+    payeReferenceNumber: String,
+    locationName: String,
+    address: String,
+    addressLine2: String,
+    city: String,
+    postcode: String,
+    country: String,
+    ukviApproved: { type: Boolean, default: false },
+    canceledAt: Date,
+}, { timestamps: true });
+
+const Location = mongoose.model("Location", locationSchema);
+
+module.exports = Location;
--- routes/superAdminRoute.js
+++ routes/superAdminRoute.js
@@ -1,14 +1,21 @@
 const Router = require('express')
 const { auth } = require('../middleware/authenticate')
 const { addCompany, getCompany, updateCompanyDetails, deleteCompany, getAllCompany } = require('../controller/superAdminController/crudCompanies')
+const { addLocation, getLocation, getAllLocation, updateLocationDetails, deleteLocation } = require('../controller/superAdminController/crudLocation')
 
 const superAdminRoute = Router()
 
-
+//company
 superAdminRoute.post('/addcompany', auth(['Superadmin']), addCompany)
 superAdminRoute.post('/getcompany/:id', auth(['Superadmin']), getCompany)
 superAdminRoute.post('/getallcompany', auth(['Superadmin']), getAllCompany)
 superAdminRoute.post('/updatecompany/:id', auth(['Superadmin']), updateCompanyDetails)
 superAdminRoute.post('/deletecompany/:id', auth(['Superadmin']), deleteCompany)
+//location
+superAdminRoute.post('/addlocation', auth(['Superadmin']), addLocation)
+superAdminRoute.post('/getlocation/:id', auth(['Superadmin']), getLocation)
+superAdminRoute.post('/getalllocation', auth(['Superadmin']), getAllLocation)
+superAdminRoute.post('/updatelocation/:id', auth(['Superadmin']), updateLocationDetails)
+superAdminRoute.post('/deletelocation/:id', auth(['Superadmin']), deleteLocation)
 
 module.exports = superAdminRoute
