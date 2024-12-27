c683e3bd0b626f74a0d34afdcd709ae9b8c350ab
HeniBalar <henishreesoftic@gmail.com>
2024-12-27 03:27:59 -0700

    remove extra old code administrator route file

--- routes/administratorRoute.js
+++ routes/administratorRoute.js
@@ -1,6 +1,5 @@
 const Router = require('express')
 const { auth } = require('../middleware/authenticate')
-const { addManager, getManager, getAllManager, updateManagerDetails, deleteManager } = require('../controller/administratorController/crudManager')
 
 const administratorRoute = Router()
 
