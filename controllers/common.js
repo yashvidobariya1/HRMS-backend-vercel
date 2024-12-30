95187944448ef5ba5aed273336a4d1249367fa01
HeniBalar <henishreesoftic@gmail.com>
2024-12-30 03:05:37 -0700

    fix conflict

--- controllers/common.js
+++ controllers/common.js
@@ -36,3 +36,24 @@ exports.login = async (req, res) => {
         return res.status(500).send({ message: error.message });
     }
 };
+
+exports.forgotPassword = async (req, res) => {
+    try {
+
+        const { email, newPassword } = req.body
+        const user = await User.findOne({
+            "personalDetails.email": email,
+            isDeleted: false
+        })
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+        user.personalDetails.password = hashedPassword
+        await user.save()
+        res.status(200).send({ message: "Password updated successfully." })
+    } catch (error) {
+        console.log('Error:', error)
+        return res.send({ message: error.message })
+    }
+}
